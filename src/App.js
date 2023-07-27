33744e16a0dea97c7e117e24c81c68451f3adb80
GM637 <gm637official@gmail.com>
2023-07-28 01:14:34 +0800

    Big Update before The Last 3 Days
    - Added SFXs
    - Added new tile types
    - Resource bar works nicely now

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import ResourceBar from './ResourceBar';
 
-
 const ROWS = 4;
 const COLS = 4;
-const MOLE_APPEAR_DURATION = 400; // Milliseconds
+const MOLE_APPEAR_DURATION = 200; // Milliseconds
 const WHACKED_DURATION = 200; // Milliseconds
 const COMBO_TIMEOUT = 1000; // Milliseconds
 
+const whackSoundurl = 'https://www.myinstants.com/media/sounds/hitmarker_2.mp3';
+const missSoundurl = 'https://www.myinstants.com/media/sounds/bamboo-hit-sound-effect.mp3';
+const multiplierSoundurl = 'https://www.myinstants.com/media/sounds/critical-hit-sounds-effect.mp3';
+const partyurl = 'https://www.myinstants.com/media/sounds/instant-dance-party-song.mp3';
+
+const missSound = new Audio(missSoundurl);
+const multiplierSound = new Audio(multiplierSoundurl);
+const partysound = new Audio(partyurl);
+
 const createEmptyGrid = () => {
-  return Array.from({ length: ROWS }, () => Array(COLS).fill(null));
+  return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+};
+
+const generateRandomMole = () => {
+  const randomMole = Math.random() < 0.6 ? 1 : Math.random() < 0.6 ? 2 : Math.random() < 0.6 ? 3 : 4; // 60% chance of 1 point mole, 20% chance of 5 points mole, 20% chance of -3 points mole
+  return randomMole;
 };
 
 const App = () => {
@@ -25,7 +38,7 @@ const App = () => {
 
   const whackedTimeoutRef = useRef(null);
   const whackedTilesRef = useRef([]);
-  
+
   const resetMultiplier = () => {
     setComboMultiplier(1);
   };
@@ -34,30 +47,60 @@ const App = () => {
     setComboRequirement(10);
   };
 
+  const playRandomPitch = (audioSource, minRate, maxRate) => {
+    const audio = new Audio(audioSource);
+
+    const randomRate = Math.random() * (maxRate - minRate) + minRate;
+    audio.playbackRate = randomRate;
+
+    audio.play();
+  };
+
   const handleTileClick = (row, col) => {
-    if (grid[row][col] === true) {
-      if (grid[row][col] !== 'whacked') {
+    partysound.play();
+    partysound.loop = true;
+
+    if (partysound.duration - partysound.currentTime < 4) {
+      partysound.volume = Math.max(0, partysound.volume - 0.04);
+    } else {
+      partysound.volume = 0.25;
+    }
+
+    partysound.volume = Math.max(0, partysound.volume);
+
+    const tileType = grid[row][col];
+
+    if (tileType > 0) {
+      if (tileType !== 'whacked') {
         setGrid((prevGrid) => {
           const newGrid = [...prevGrid];
-          newGrid[row][col] = 'whacked';
+          if (tileType === 4) {
+            // For type 4, decrement the value, but don't go below 0
+            newGrid[row][col] = Math.max(0, tileType - 1);
+          } else {
+            newGrid[row][col] = 'whacked';
+          }
           return newGrid;
         });
-  
-        setScore((prevScore) => prevScore + comboMultiplier);
+
+        const pointsEarned = tileType === 4 ? 1 : tileType; // Type 4 mole (takes multiple clicks) earns 1 point, other types earn their respective points
+        setScore((prevScore) => prevScore + ( pointsEarned * comboMultiplier));
         setCombo((prevCombo) => {
           if (prevCombo >= comboMultiplier * 3) {
             setComboMultiplier((prevMultiplier) => prevMultiplier * 2);
+            multiplierSound.play();
           }
-          return prevCombo + 1;
+          return prevCombo + ( pointsEarned * comboMultiplier);
         });
-  
+
+        playRandomPitch(whackSoundurl, 0.8, 1.2);
+
         if (comboTimer === null) {
           setCombo(1);
-          setComboTimer(COMBO_TIMEOUT); // Set the combo timer to 5000ms (5 seconds) if comboTimer is null
+          setComboTimer(COMBO_TIMEOUT);
         } else {
-          // Calculate the new combo timer duration based on the offset
-          setComboRequirement(comboMultiplier * 3); // Increase combo requirement for the next multiplier
-          setComboTimer(COMBO_TIMEOUT-(comboMultiplier-1)/2*50);
+          setComboRequirement(comboMultiplier * 3);
+          setComboTimer(Math.round(COMBO_TIMEOUT - (comboMultiplier - 1) / 4 * 50));
         }
 
         if (comboTimer <= 0) {
@@ -66,12 +109,12 @@ const App = () => {
           resetComboRequirement();
           setComboTimer(COMBO_TIMEOUT);
         }
-  
+
         if (whackedTimeoutRef.current) clearTimeout(whackedTimeoutRef.current);
         whackedTimeoutRef.current = setTimeout(() => {
           setGrid((prevGrid) => {
             const newGrid = [...prevGrid];
-            newGrid[row][col] = null;
+            newGrid[row][col] = 0; // Reset the tile back to the default state (null)
             return newGrid;
           });
           whackedTilesRef.current = whackedTilesRef.current.filter(
@@ -80,10 +123,9 @@ const App = () => {
         }, WHACKED_DURATION);
 
         setPopupMessage('');
-  
-        const pointsEarned = comboMultiplier > 1 ? `+${comboMultiplier} points` : '+1 point';
-        setPopupMessage(pointsEarned);
-  
+        const pointsMessage = pointsEarned > 0 ? `+${pointsEarned * comboMultiplier} points` : `${pointsEarned * comboMultiplier} points`;
+        setPopupMessage(pointsMessage);
+
         if (popupTimeout) clearTimeout(popupTimeout);
         setPopupTimeout(
           setTimeout(() => {
@@ -92,53 +134,46 @@ const App = () => {
         );
       }
     } else {
-      // Combo was reset, so reset the multiplier and combo requirement
-      setCombo(0);
-      resetMultiplier();
-      resetComboRequirement();
-  
-      // Clear the existing popup message, if any
       setPopupMessage('MISSED');
+      missSound.play();
 
-    // Clear the whackedTimeout if a new tile is clicked
-    if (whackedTimeoutRef.current) clearTimeout(whackedTimeoutRef.current);
-
-    // Clear the whackedTimeoutRef.current after a small delay to avoid race condition
-    setTimeout(() => {
-      whackedTimeoutRef.current = null;
-    }, 100);
-  }
-
-  // Check if the tile was previously whacked but not clicked again
-  if (grid[row][col] === 'whacked') {
-    // Add the whacked tile to the ref to track it
-    whackedTilesRef.current.push({ row, col });
-
-    // Reset the tile to grey after a certain duration (e.g., 500ms)
-    setTimeout(() => {
-      if (whackedTilesRef.current.some((tile) => tile.row === row && tile.col === col)) {
+      if (whackedTimeoutRef.current) clearTimeout(whackedTimeoutRef.current);
+      whackedTimeoutRef.current = setTimeout(() => {
         setGrid((prevGrid) => {
           const newGrid = [...prevGrid];
-          newGrid[row][col] = null;
+          newGrid[row][col] = 0;
           return newGrid;
         });
         whackedTilesRef.current = whackedTilesRef.current.filter(
           (tile) => !(tile.row === row && tile.col === col)
         );
-      }
-    }, 500);
-  }
-};
-  // Update the combo timer every millisecond
-  useEffect(() => {
-    if (comboTimer !== null) {
-      const interval = setInterval(() => {
-        setComboTimer((prevTimer) => Math.max(0, prevTimer - 1));
-      }, 1);
+      }, WHACKED_DURATION);
 
-      return () => clearInterval(interval);
+      setCombo(0);
+      resetMultiplier();
+      resetComboRequirement();
+
+      if (comboTimer <= 0) {
+        setComboTimer(COMBO_TIMEOUT);
+      }
     }
-  }, [comboTimer]);
+
+    if (grid[row][col] === 'whacked') {
+      whackedTilesRef.current.push({ row, col });
+      setTimeout(() => {
+        if (whackedTilesRef.current.some((tile) => tile.row === row && tile.col === col)) {
+          setGrid((prevGrid) => {
+            const newGrid = [...prevGrid];
+            newGrid[row][col] = 0;
+            return newGrid;
+          });
+          whackedTilesRef.current = whackedTilesRef.current.filter(
+            (tile) => !(tile.row === row && tile.col === col)
+          );
+        }
+      }, WHACKED_DURATION);
+    }
+  };
 
   useEffect(() => {
     const placeMole = () => {
@@ -147,8 +182,8 @@ const App = () => {
 
       setGrid((prevGrid) => {
         const newGrid = [...prevGrid];
-        if (newGrid[randomRow][randomCol] !== true) {
-          newGrid[randomRow][randomCol] = true;
+        if (newGrid[randomRow][randomCol] === 0) {
+          newGrid[randomRow][randomCol] = generateRandomMole();
         }
         return newGrid;
       });
@@ -170,7 +205,7 @@ const App = () => {
           row.map((col, colIndex) => (
             <div
               key={`${rowIndex}-${colIndex}`}
-              className={`tile ${col === 'whacked' ? 'mole-whacked' : col ? 'mole' : ''}`}
+              className={`tile ${col === 'whacked' ? 'mole-whacked' : col ? `mole-${col}` : ''}`}
               onClick={() => handleTileClick(rowIndex, colIndex)}
             >
               {col === 'whacked' ? 'POW!' : ''}
@@ -184,17 +219,17 @@ const App = () => {
           value={combo}
           maxValue={comboRequirement}
           color="#6dbb1a"
-          prefixText="Combo Progress:" // Optional prefix text
+          prefixText="Combo Progress:"
           suffixText={` (x${comboMultiplier})`}
         />
         <ResourceBar
-          value={comboTimer}
-          maxValue={COMBO_TIMEOUT-(comboMultiplier-1)/2*50}
+          value={Math.round(comboTimer)}
+          maxValue={Math.round(COMBO_TIMEOUT - (comboMultiplier - 1) / 4 * 50)}
           color="#1a8abd"
-          prefixText="Time Left:" // Optional prefix text
-          suffixText=" s" // Optional suffix text
-          animationSpeed={0.2} // Set the animation speed to 1.5 seconds
-          showText // Display the text inside the bar
+          prefixText="Time Left:"
+          suffixText=" ms"
+          animationSpeed={0.2}
+          showText
         />
       </div>
       <div className={`popup-message ${popupMessage ? 'show-popup' : ''}`}>{popupMessage}</div>
